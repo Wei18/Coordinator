a2020220599ec20eeca1fe6c58d3e1da2d0fc7c9
ZhiWei Cao <41205mw@gmail.com>
2018-10-31 19:43:36 +0800

    Initial Commit

---
+++ Coordinator/Coordinator.h
@@ -0,0 +1,19 @@
+//
+//  Coordinator.h
+//  Coordinator
+//
+//  Created by ZhiWei Cao on 10/31/18.
+//  Copyright © 2018 ZhiWei Cao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Coordinator.
+FOUNDATION_EXPORT double CoordinatorVersionNumber;
+
+//! Project version string for Coordinator.
+FOUNDATION_EXPORT const unsigned char CoordinatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Coordinator/PublicHeader.h>
+
+
